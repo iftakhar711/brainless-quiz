fa3ff977a2e5ac61835b318bad3e0e27e022573c
iftakhar711 <iftakharudiin80@gmail.com>
2022-10-11 13:00:09 +0600

    first commit

---
+++ src/Components/Routes.js
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./home/Home";
+import Root from "./Root";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root></Root>,
+        children: [
+            { path: '/', element: <Home></Home> },
+            { path: '/', element: <Home></Home> },
+            { path: '/', element: <Home></Home> }
+        ]
+    }
+])
